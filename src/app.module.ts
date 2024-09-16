1261c066e3663f83bd78c36ec28f3d2eaa838741
Muhammad Aneeque <aneeq.khatri@celeritasdigital.com>
2024-09-16 11:39:01 +0500

    initial structure

---
+++ src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { ProductsModule } from './products/products.module';
+import { OrdersModule } from './orders/orders.module';
+import { ReviewsModule } from './reviews/reviews.module';
+
+@Module({
+  imports: [AuthModule, UsersModule, ProductsModule, OrdersModule, ReviewsModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
